d12b90f4cad5a9fc8ecac7dd2a0d3dabc3e49f21
alexandre <alexandre@katrid.com>
2016-04-12 20:42:21 -0300

    Add Report.print method (print Html directly)

--- test/test.js
+++ test/test.js
@@ -1,4 +1,12 @@
 var testReport = {
+  totals: [
+    {
+      func: 'SUM',
+      name: 'SUM1',
+      band: 'dataBand1',
+      expression: 'row'
+    }
+  ],
   pages: [
     {
       marginTop: 20,
@@ -7,10 +15,9 @@ var testReport = {
       marginLeft: 20,
       children: [
         {
-          type: 'DataBand',
-          name: 'dataBand1',
-          rowCount: 100000,
-          height: 30,
+          type: 'ReportTitle',
+          name: 'reportTitle1',
+          height: 35,
           children: [
             {
               type: 'Text',
@@ -18,13 +25,42 @@ var testReport = {
               top: 0,
               height: 18,
               width: 100,
-              text: '{{row}} {{10*2.21}}'
+              text: 'Report Title'
+            }
+          ]
+        },
+        {
+          type: 'GroupHeader',
+          expression: 'row.toString()[0]',
+          children: [
+            {
+              type: 'Text',
+              left: 0,
+              top: 0,
+              text: 'GROUP BY: row.toString()[0] -> {{row.toString()[0]}}'
+            }
+          ],
+          height: 20
+        },
+        {
+          type: 'DataBand',
+          name: 'dataBand1',
+          rowCount: 10000,
+          height: 30,
+          children: [
+            {
+              type: 'Text',
+              left: 0,
+              top: 0,
+              height: 18,
+              width: 350,
+              text: '{{row}} {{10*2.21}} - SUM: {{SUM1}}'
             }, {
               type: 'Text',
               left: 100,
-              top: 100,
+              top: 50,
               height: 18,
-              width: 100,
+              width: 300,
               border: {
                 left: true,
                 bottom: true,
@@ -34,6 +70,21 @@ var testReport = {
                 color: '#b1b1b1'
               },
               text: 'My 2nd text object'
+            }
+          ]
+        },
+        {
+          type: 'PageFooter',
+          name: 'pageFooter1',
+          height: 35,
+          children: [
+            {
+              type: 'Text',
+              left: 0,
+              top: 0,
+              height: 18,
+              width: 100,
+              text: 'Page Footer'
             }
           ]
         }
